0c4ee2537e1da4cad48c9d0f81f134bc99ad4141
malek mekdashi <malekmekdashi@gmail.com>
2022-10-22 22:19:19 -0700

    added handlebars and login, profile and updated a few things

--- models/Blog.js
+++ models/Blog.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 
 class Blog extends Model {}
 
-Comment.init(
+Blog.init(
     {
         id: {
             type: DataTypes.INTEGER,
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,23 @@
 module.exports = {
-    get_emoji: () => {
-        const randomNum = Math.randon();
-        
-        if (randomNum > 0.7) {
-            return `<span for="img" aria-label=`
-        }
-    }
-}
+  format_time: (date) => {
+      // format large numbers with commas
+      return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+      new Date(date).getFullYear() + 5
+    }`;
+  },
+  get_emoji: () => {
+      const randomNum = Math.random();
+  
+      // Return a random emoji
+      if (randomNum > 0.7) {
+        return `<span for="img" aria-label="lightbulb">💡</span>`;
+      } else if (randomNum > 0.4) {
+        return `<span for="img" aria-label="laptop">💻</span>`;
+      } else {
+        return `<span for="img" aria-label="gear">⚙️</span>`;
+      }
+    },
+  };
